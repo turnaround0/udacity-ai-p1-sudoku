6fe59cf523eeccdccbd8d2915a0599a1710762c9
Jaehwan Park <turnaround0@gmail.com>
2019-03-12 20:14:55 +0900

    Implement solution and add readme file for Sudoku
    A solution of Sudoku was implemented on solution.py file.
    Also, the explanation of algorihm was written on README.md file.

--- README.md
+++ README.md
@@ -1,80 +1,54 @@
-# Solve Sudoku with AI
+# Artificial Intelligence Nanodegree
+## Introductory Project: Diagonal Sudoku Solver
 
-## Synopsis
+# Question 1 (Naked Twins)
+Q: How do we use constraint propagation to solve the naked twins problem?  
+A: With naked twin strategy, we can reduce possibility of other box values in a unit.
+   In a unit, there are all 1 to 9 numbers only once.
+   If two boxes have same twin values like 23, other boxes cannot have the digit 2 and 3.
+   Because those two boxes should have (2 and 3) or (3 and 2).
+   So, we can remove 2 or 3 digit on all other boxes in the unit.
+   We can apply this constraint to remove digits of the naked twins on other boxes in same unit.
 
-In this project, students will extend the Sudoku-solving agent developed in the classroom lectures to solve _diagonal_ Sudoku puzzles. A diagonal Sudoku puzzle is identical to traditional Sudoku puzzles with the added constraint that the boxes on the two main diagonals of the board must also contain the digits 1-9 in each cell (just like the rows, columns, and 3x3 blocks).
+# Question 2 (Diagonal Sudoku)
+Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
+A: Two diagonal units can be created like [A1, B2, ... I9], [A9, B8, ..., I1].
+   This diagonal unit will be added in unit list for new constraint.
+   Like other units, on diagonal unit, there are all 1 to 9 numbers only once.
+   So, a box has new peer of these diagonal units.
+   Whenever applying stratgies: elmination, only choice and others,
+   this constraint will be applied, and reduce possibility of other box values in diagonal units.
 
-## Instructions
+### Install
 
-Follow the instructions in the classroom lesson to install and configure the AIND [Anaconda](https://www.continuum.io/downloads) environment. That environment includes several important packages that are used for the project. 
+This project requires **Python 3**.
 
-**YOU ONLY NEED TO WRITE CODE IN `solution.py`.**
+We recommend students install [Anaconda](https://www.continuum.io/downloads), a pre-packaged Python distribution that contains all of the necessary libraries and software for this project. 
+Please try using the environment we provided in the Anaconda lesson of the Nanodegree.
 
+##### Optional: Pygame
 
-## Quickstart Guide
+Optionally, you can also install pygame if you want to see your visualization. If you've followed our instructions for setting up our conda environment, you should be all set.
 
-### Activate the aind environment (OS X or Unix/Linux)
-    
-    `$ source activate aind`
+If not, please see how to download pygame [here](http://www.pygame.org/download.shtml).
 
-### Activate the aind environment (Windows)
+### Code
 
-    `> activate aind`
+* `solution.py` - You'll fill this in as part of your solution.
+* `solution_test.py` - Do not modify this. You can test your solution by running `python solution_test.py`.
+* `PySudoku.py` - Do not modify this. This is code for visualizing your solution.
+* `visualize.py` - Do not modify this. This is code for visualizing your solution.
 
-### Run the code & visualization
+### Visualizing
 
-    `(aind)$ python solution.py`
+To visualize your solution, please only assign values to the values_dict using the `assign_value` function provided in solution.py
 
-### Run the local test suite
+### Submission
+Before submitting your solution to a reviewer, you are required to submit your project to Udacity's Project Assistant, which will provide some initial feedback.  
 
-    `(aind)$ python -m unittest -v`
+The setup is simple.  If you have not installed the client tool already, then you may do so with the command `pip install udacity-pa`.  
 
-### Run the remote test suite & submit the project
+To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login) for alternate login instructions.
 
-    `(aind)$ udacity submit`
+This process will create a zipfile in your top-level directory named sudoku-<id>.zip.  This is the file that you should submit to the Udacity reviews system.
 
-
-## Coding
-
-You must complete the required functions in the 'solution.py' file (copy in code from the classroom where indicated, and add or extend with new code as described below). The `test_solution.py` file includes a few unit tests for local testing (See the unittest module for information on getting started.), but the primary mechanism for testing your code is the Udacity Project Assistant command line utility described in the next section.
-
-YOU SHOULD EXPECT TO MODIFY OR WRITE YOUR OWN UNIT TESTS AS PART OF COMPLETING THIS PROJECT. The Project Assistant test suite is not shared with students. Writing your own tests leads to a deeper understanding of the project.
-
-1. Run the following command from inside the project folder in your terminal to verify that your system is properly configured for the project. You should see feedback in the terminal about failed test cases -- which makes sense because you haven't implemented any code yet. You will reuse this command later to execute your **local** test cases.
-
-    `$ python -m unittest -v`
-
-1. Run the following command from inside the project folder in your terminal to verify that the Udacity-PA tool is installed properly. You should see a list of failed test cases -- which is good because you haven't implemented any code yet. You will reuse this command later to execute the **remote** test cases and complete the project.
-
-    `$ udacity submit`
-
-1. Add the two new diagonal units to the `unitlist` at the top of solution.py. Re-run the local tests with `python -m unittest` to confirm your solution. 
-
-1. Copy your code from the classroom for the `eliminate()`, `only_choice()`, `reduce_puzzle()`, and `search()` into the corresponding functions in the `solution.py` file.
-
-1. Implement the `naked_twins()` function, and update `reduce_puzzle()` to call it (along with the other existing strategies). Re-run the local tests with `python -m unittest -v` to confirm your solution.
-
-1. Write your own test cases to further test your code. Re-run the remote tests with `udacity submit` to confirm your solution. If any of the remote test cases fail, use the feedback to write new local test cases that you can use for debugging.
-
-
-## Submission
-
-To submit your code, run `udacity submit` from a terminal in the top-level directory of this project. You will be prompted for a username and password the first time the script is run. If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login) for alternate login instructions.
-
-The Udacity-PA CLI tool is automatically installed with the AIND conda environment provided in the classroom, but you can also install it manually by running `pip install udacity-pa`. You can submit your code for scoring by running `udacity submit`. The project assistant server has a collection of unit tests that it will execute on your code, and it will provide feedback on any successes or failures. You must pass all test cases in the project assistant to pass the project.
-
-Once your project passes all test cases on the Project Assistant, submit the zip file created by the `udacity submit` command in the classroom to automatically receive credit for the project. NOTE: You will not receive personalized feedback for this project on submissions that pass all test cases, however, all other projects in the term do provide personalized feedback on both passing & failing submissions.
-
-
-## Troubleshooting
-
-Your classroom mentor may be able to provide some guidance on the project, but the [discussion forums](https://discussions.udacity.com/c/nd889-intro-sudoku) or [slack team](https://ai-nd.slack.com) (especially the #p-sudoku channel) should be your primary support resources. The instructors hold regularly scheduled office hours in the Slack community. (The schedule is posted in the description of the #office-hours channel.)
-
-Contact ai-support@udacity.com if you don't have access to the forums or Slack team.
-
-
-## Visualization
-
-**Note:** The `pygame` library is required to visualize your solution -- however, the `pygame` module can be troublesome to install and configure. It should be installed by default with the AIND conda environment, but it is not reliable across all operating systems or versions. Please refer to the pygame documentation [here](http://www.pygame.org/download.shtml), or discuss among your peers in the slack group or discussion forum if you need help.
-
-Running `python solution.py` will automatically attempt to visualize your solution, but you mustuse the provided `assign_value` function (defined in `utils.py`) to track the puzzle solution progress for reconstruction during visuzalization.
--- solution.py
+++ solution.py
@@ -1,166 +1,219 @@
+assignments = []
 
-from utils import *
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [s + t for s in A for t in B]
 
+def collect(A, B):
+    return list(map(lambda x: x[0] + x[1], zip(A, B)))
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+boxes = cross(rows, cols)
 
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+diag_units = [collect(rows, cols), collect(rows, cols[::-1])]
+unitlist = row_units + column_units + square_units + diag_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[])) - set([s])) for s in boxes)
 
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+def assign_value(values, box, value):
+    """
+    Please use this function to update your values dictionary!
+    Assigns a value to a given box. If it updates the board record it.
+    """
 
+    # Don't waste memory appending actions that don't actually change any values
+    if values[box] == value:
+        return values
 
-# Must be called after all units (including diagonals) are added to the unitlist
-units = extract_units(unitlist, boxes)
-peers = extract_peers(units, boxes)
-
+    values[box] = value
+    if len(value) == 1:
+        assignments.append(values.copy())
+    return values
 
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
+    Remove digits of twins on other boxes in a unit.
+    Args:
+        values(dict): a dictionary of the form {'box_name': '123456789', ...}
 
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
-
-    Returns
-    -------
-    dict
-        The values dictionary with the naked twins eliminated from peers
-
-    Notes
-    -----
-    Your solution can either process all pairs of naked twins from the input once,
-    or it can continue processing pairs of naked twins until there are no such
-    pairs remaining -- the project assistant test suite will accept either
-    convention. However, it will not accept code that does not process all pairs
-    of naked twins from the original input. (For example, if you start processing
-    pairs of twins and eliminate another pair of twins before the second pair
-    is processed then your code will fail the PA test suite.)
-
-    The first convention is preferred for consistency with the other strategies,
-    and because it is simpler (since the reduce_puzzle function already calls this
-    strategy repeatedly).
+    Returns:
+        the values dictionary with the naked twins eliminated from peers.
     """
-    # TODO: Implement this function!
-    raise NotImplementedError
 
+    # Find all instances of naked twins
+    # Eliminate the naked twins as possibilities for their peers
 
-def eliminate(values):
-    """Apply the eliminate strategy to a Sudoku puzzle
+    # Find all twin boxes and make set of tuple with same value
+    doubles = [box for box in values if len(values[box]) == 2]
+    twins = {tuple(sorted([s, t])) for s in doubles for t in doubles if s != t and values[s] == values[t]}
 
-    The eliminate strategy says that if a box has a value assigned, then none
-    of the peers of that box can have the same value.
+    # Store twin values before applying this strategy,
+    # because box values of the twins can be changed due to this strategy
+    twin_digits = dict()
+    for twin in twins:
+        twin_digits[twin[0]] = values[twin[0]]
 
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
-
-    Returns
-    -------
-    dict
-        The values dictionary with the assigned values eliminated from peers
-    """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
-
-
-def only_choice(values):
-    """Apply the only choice strategy to a Sudoku puzzle
-
-    The only choice strategy says that if only one box in a unit allows a certain
-    digit, then that box must be assigned that digit.
-
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
-
-    Returns
-    -------
-    dict
-        The values dictionary with all single-valued boxes assigned
-
-    Notes
-    -----
-    You should be able to complete this function by copying your code from the classroom
-    """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
-
-
-def reduce_puzzle(values):
-    """Reduce a Sudoku puzzle by repeatedly applying all constraint strategies
-
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
-
-    Returns
-    -------
-    dict or False
-        The values dictionary after continued application of the constraint strategies
-        no longer produces any changes, or False if the puzzle is unsolvable 
-    """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
-
-
-def search(values):
-    """Apply depth first search to solve Sudoku puzzles in order to solve puzzles
-    that cannot be solved by repeated reduction alone.
-
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
-
-    Returns
-    -------
-    dict or False
-        The values dictionary with all boxes assigned or False
-
-    Notes
-    -----
-    You should be able to complete this function by copying your code from the classroom
-    and extending it to call the naked twins strategy.
-    """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
-
-
-def solve(grid):
-    """Find the solution to a Sudoku puzzle using search and constraint propagation
-
-    Parameters
-    ----------
-    grid(string)
-        a string representing a sudoku grid.
-        
-        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-
-    Returns
-    -------
-    dict or False
-        The dictionary representation of the final sudoku grid or False if no solution exists.
-    """
-    values = grid2values(grid)
-    values = search(values)
+    # For each unit, apply the naked twins strategy
+    for unit in unitlist:
+        for twin in twins:
+            if twin[0] in unit and twin[1] in unit:
+                digits = twin_digits[twin[0]]
+                for box in unit:
+                    # remove digits of twins on other boxes in same unit
+                    if values[box] != digits and box not in twins:
+                        assign_value(values, box, values[box].replace(digits[0], '').replace(digits[1], ''))
     return values
 
 
-if __name__ == "__main__":
+def grid_values(grid):
+    """
+    Convert grid into a dict of {square: char} with '123456789' for empties.
+    Args:
+        grid(string) - A grid in string form.
+    Returns:
+        A grid in dictionary form
+            Keys: The boxes, e.g., 'A1'
+            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
+    """
+    # '.' means empty cell, so '.' should be converted to '123456789'.
+    return dict(zip(boxes, [value.replace('.', '123456789') for value in grid]))
+
+def display(values):
+    """
+    Display the values as a 2-D grid.
+    Args:
+        values(dict): The sudoku in dictionary form
+    """
+    if values:
+        width = 1 + max(len(values[s]) for s in boxes)
+        line = '+'.join(['-' * (width * 3)] * 3)
+        # print column line after 3 and 6, and row line after C and F.
+        for r in rows:
+            print(''.join(values[r + c].center(width) + ('|' if c in '36' else '') for c in cols))
+            if r in 'CF':
+                print(line)
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    # Find box with only one digit
+    singles = [box for box in values.keys() if len(values[box]) == 1]
+
+    # Remove the digit of single box on all its peers.
+    for box in singles:
+        for peer in peers[box]:
+            if values[box] in values[peer]:
+                assign_value(values, peer, values[peer].replace(values[box], ''))
+    return values
+
+def only_choice(values):
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    for unit in unitlist:
+        for digit in '123456789':
+            digit_box = [box for box in unit if digit in values[box]]
+            # If the digit exists on only ne box, the digit is assigned to the box.
+            if len(digit_box) == 1:
+                assign_value(values, digit_box[0], digit)
+    return values
+
+def reduce_puzzle(values):
+    """
+    Iterate eliminate(), only_choice(), and naked_twins().
+    If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+
+    Args:
+        values: A sudoku in dictionary form.
+    Returns:
+        The resulting sudoku in dictionary form.
+    """
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        values = eliminate(values)
+        values = only_choice(values)
+        values = naked_twins(values)
+
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+def search(values):
+    """
+    Using depth-first search and propagation, try all possible values.
+
+    Args:
+        values: A sudoku in dictionary form.
+    Returns:
+        The resulting sudoku in dictionary form. False if no solution exists.
+    """
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False
+    if all(len(values[box]) == 1 for box in values):
+        return values
+
+    # Choose one of the unfilled squares with the fewest possibilities
+    _, box = min((len(values[box]), box) for box in boxes if len(values[box]) > 1)
+
+    # Now use recursion to solve each one of the resulting sudokus,
+    # and if one returns a value (not False), return that answer!
+    for digit in values[box]:
+        new_values = values.copy()
+        assign_value(new_values, box, digit)
+        new_values = search(new_values)
+        if new_values:
+            return new_values
+
+    return False
+
+def solve(grid):
+    """
+    Find the solution to a Sudoku grid.
+    Args:
+        grid(string): a string representing a sudoku grid.
+            Example: '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    Returns:
+        The dictionary representation of the final sudoku grid. False if no solution exists.
+    """
+    # Convert grid to a sudoku in dictionary form, and search the solution of the sudoku
+    return search(grid_values(grid))
+
+if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-    display(grid2values(diag_sudoku_grid))
-    result = solve(diag_sudoku_grid)
-    display(result)
+    display(solve(diag_sudoku_grid))
 
     try:
-        import PySudoku
-        PySudoku.play(grid2values(diag_sudoku_grid), result, history)
+        from visualize import visualize_assignments
+        visualize_assignments(assignments)
 
     except SystemExit:
         pass
